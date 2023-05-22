c7de4365af6e03aca659b97e64374f25f6161d1d
Jananiviswa15 <73423520+Jananiviswa15@users.noreply.github.com>
2023-05-22 12:02:55 +0530

    constructor

---
+++ Building.java
@@ -0,0 +1,69 @@
+package oops;
+
+public class Building {
+
+	String ownerName;
+	static String builderName;
+	static {
+		builderName = "ABC";
+//		displayBuilderName();
+//		Building owner1 = new Building();
+//		owner1.displayOwnerDetails();
+	}
+	boolean isSmartHome;
+	int noOfRooms;
+
+	Building(){ // no arg cons
+		System.out.println(" i am constr");
+//		builderName = "hello";
+//		displayBuilderName();
+//		displayOwnerDetails();
+		
+	}
+	
+	
+	//owner2
+	//this --> current class object ref
+			//"SURAJ", 5, true
+	Building(String name, int noOfRooms , boolean isSmartHome){
+		this.isSmartHome = isSmartHome;  //surajBuilding.isSmartHome = true;
+		this.noOfRooms = noOfRooms; //surajBuilding.noOfRooms = 5
+		this.ownerName = name;
+	}
+	
+	Building(int noOfRooms){
+		this.noOfRooms = noOfRooms; //achalBuilding.noOfRooms = 5
+	}
+	
+	void displayOwnerDetails() {
+		System.out.println("i am an owner");
+	}
+	
+	static void displayBuilderName() {
+		System.out.println(builderName);
+	}
+	
+	void displayBuildingDetails() {
+		System.out.println(builderName+ " "+ noOfRooms +" "+ isSmartHome+ " "+ ownerName);
+	}
+	public static void main(String args[]) {
+
+		Building owner1 = new Building();
+		//dc --> jvm --> def val , called auto by jvm
+		
+		Building owner2 = new Building("akshatha", 3 , false);
+		////dc --> jvm --> def val , called auto by jvm
+		
+		
+		Building surajBuilding = new Building("SURAJ", 5, true);
+		surajBuilding.noOfRooms = 10;
+		
+		Building achalBuilding = new Building(5);
+		
+		owner1.displayBuildingDetails();
+		owner2.displayBuildingDetails();
+		surajBuilding.displayBuildingDetails();
+		achalBuilding.displayBuildingDetails();
+		
+	}
+}
