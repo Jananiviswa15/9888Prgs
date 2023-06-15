06f4069517c524d6ce6565d50ed53b65a8a70328
Jananiviswa15 <73423520+Jananiviswa15@users.noreply.github.com>
2023-06-15 11:42:49 +0530

    decorator

---
+++ Decorator/ShapeDecorator.java
@@ -0,0 +1,12 @@
+package Decorator;
+
+public abstract class ShapeDecorator implements Shape {
+	protected Shape decoratedShape;  //circle
+	public ShapeDecorator(Shape decoratedShape){
+		this.decoratedShape = decoratedShape; //new Circle()
+		//redCircle.decorated shape = new Circle()
+	}
+	public void draw(){
+		decoratedShape.draw();
+	}
+}
