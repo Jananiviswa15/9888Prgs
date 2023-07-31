3a0e8fb39bf86df4c390eda8c6e3004124033236
Jananiviswa15 <73423520+Jananiviswa15@users.noreply.github.com>
2023-07-31 10:30:00 +0530

    js1

---
+++ FirstFile.js
@@ -0,0 +1,78 @@
+// let radiusOfTheCircle = 10;
+// const piValue = 3.14;
+// let area;
+
+// area = radiusOfTheCircle * radiusOfTheCircle * piValue;
+
+// console.log(area);
+
+
+let wholeNum = 4545;
+let negativeNum = -4245;
+let floatingNum = 4255.324434;
+let exp = 343e3;
+
+console.log(typeof wholeNum);
+console.log(typeof negativeNum);
+console.log(typeof floatingNum);
+console.log(typeof exp);
+
+let name = "dskjfkdf";
+let username = 'dsff';
+let sentence = "good mrg student's";
+
+console.log(typeof name);
+console.log(typeof username);
+console.log(typeof sentence);
+
+let isMarried = true;
+let isSleeping = false;
+console.log(typeof isMarried);
+console.log(typeof isSleeping);
+
+let studentMarks = null;
+console.log(studentMarks);
+
+let addressOfTheStdnt;
+console.log(addressOfTheStdnt);
+
+
+console.log("hello all\ngood mrg\tpls\bss")
+
+let num1 = 10;
+let num2 = 90;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1/num2);
+console.log(num1*num2);
+console.log(num1%num2);
+
+let num3 = 89;
+let num4 = 90;
+
+console.log(num3 > num4);
+console.log(num3 < num4);
+console.log(num3 >= num4);
+console.log(num3 <= num4);
+
+console.log(num3 == num4);  //value
+
+num3 += 10;  //99
+num3 -= 5; //94
+
+num3++; //94
+++num3  //96
+console.log(num3) //96
+
+console.log(89 / "hi all");
+
+console.log(typeof (89 / "hi all"));
+
+console.log(typeof ( "hi all" * 90));
+
+
+let number1 = 90;
+let number2 = 900;
+console.log(number1 > number2 && number1 != 0);
+console.log(number1 > number2 || number1 != 0);
+console.log(!isMarried);
