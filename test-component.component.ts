6f88c148c60807e83dc7b4c1375282f632cbefbc
Jananiviswa15 <73423520+Jananiviswa15@users.noreply.github.com>
2023-08-04 12:05:03 +0530

    selector types, inlinetemp,inlinecss, interpolation

--- test-component.component.ts
+++ test-component.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-test-component',
-  templateUrl: './test-component.component.html',
-  styleUrls: ['./test-component.component.css']
+  selector: '[app-test-component]',
+ /* templateUrl: './test-component.component.html',*/
+ template : `<div> hi all of you </div>
+ <h2> heading2 </h2>
+ <span> {{name}}</span>
+<p> {{10+10}}</p>
+ <div> {{"hello"+  name}}</div>
+ <p> {{name.length}}</p>
+ <p> {{name.toUpperCase()}} </p>
+ <section>{{ sayHi()}}</section>
+ <p> {{siteUrl}} </p>
+ `,
+ styles: ['div { color: red; }']
 })
 export class TestComponentComponent {
 
+  public name = 'JanaNN'; 
+  
+
+  public siteUrl = window.location.href;
+  sayHi(){
+    return this.name.toLowerCase();
+  }
 }
